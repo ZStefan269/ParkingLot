fe22c47abb2b15294116eab7ac7fa1bb0080c8f6
ZStefan269 <stefanzmaranda269@gmail.com>
2022-12-12 11:37:27 +0200

    Lab 6 (Parking Lot 8): "CRUD for cars"

--- src/main/java/com/parking/parkinglot/ejb/CarsBean.java
+++ src/main/java/com/parking/parkinglot/ejb/CarsBean.java
@@ -2,6 +2,7 @@ package com.parking.parkinglot.ejb;
 
 import com.parking.parkinglot.common.CarDto;
 import com.parking.parkinglot.entities.Car;
+import com.parking.parkinglot.entities.User;
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
@@ -9,29 +10,26 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-
 @Stateless
+
 public class CarsBean {
     private static final Logger LOG = Logger.getLogger(CarsBean.class.getName());
-
     @PersistenceContext
-    EntityManager entityManeger;
-    public List<CarDto> findallCars(){
+    EntityManager entityManager;
+
+    public List<CarDto> findAllCars() {
         LOG.info("findAllCars");
         try {
-            TypedQuery<Car> typedQuery =entityManeger.createQuery("SELECT c from Car c",Car.class );
-            List<Car> cars=typedQuery.getResultList();
+            TypedQuery<Car> typedQuery =
+                    entityManager.createQuery("SELECT c FROM Car c", Car.class);
+            List<Car> cars = typedQuery.getResultList();
             return copyCarsToDto(cars);
-        }
-        catch (Exception ex)
-        {
-            throw new EJBException();
+        } catch (Exception ex) {
+            throw new EJBException(ex);
         }
     }
 
@@ -42,4 +40,43 @@ public class CarsBean {
                 .map(x->new CarDto(x.getId(),x.getLICENSEPLATE(),x.getPARKINGSPOT(),x.getOwner().getUsername())).collect(Collectors.toList());
         return carDto;
     }
+    public void createCar(String licensePlate,String parkingSpot,Long userId){
+        LOG.info("createCar");
+        Car car=new Car();
+        car.setLICENSEPLATE(licensePlate);
+        car.setPARKINGSPOT(parkingSpot);
+        User user=entityManager.find(User.class,userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+        entityManager.persist(car);
+    }
+
+    public CarDto  findById(Long carId){
+
+        Car car=entityManager.find(Car.class,carId);
+        CarDto cars=new CarDto(car.getId(),car.getLICENSEPLATE(), car.getPARKINGSPOT(), car.getOwner().getUsername()) ;
+
+        return cars;
+
+    }
+
+    public void updateCar(Long carId,String licensePlate,String parkingSpot,Long userId){
+        LOG.info("updateCar");
+        Car car=entityManager.find(Car.class,carId);
+        car.setLICENSEPLATE(licensePlate);
+        car.setPARKINGSPOT(parkingSpot);
+        User oldUser=car.getOwner();
+        oldUser.getCars().remove(car);
+        User user=entityManager.find(User.class,userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+    }
+    public void deleteCarsByIds(Collection<Long> carIds){
+        LOG.info("deleteCarsByIds");
+        for(Long carId:carIds){
+            Car car=entityManager.find(Car.class,carId);
+            entityManager.remove(car);
+        }
+    }
+
 }
